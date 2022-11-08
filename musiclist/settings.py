d8772e6313f865a20584ab528dc8c568f03f4f44
kbm1933 <wjsalsgh456@gmail.com>
2022-11-08 09:20:46 +0900

    [fix] secret_key 분리

--- musiclist/settings.py
+++ musiclist/settings.py
@@ -1,9 +1,23 @@
+import json
+import os
 from pathlib import Path
 from datetime import timedelta
-
+from django.core.exceptions import ImproperlyConfigured
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 DEBUG = True
 
